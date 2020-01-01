77d845dfa6e3423034bacdd39290ed00dcd285eb
Kate Summerill <kaitlynsummerill@gmail.com>
2020-01-01 18:29:19 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -18,9 +18,10 @@ class Dog
   end
 
   def self.print_all
-    @@all.each do |name|
-      puts name
-    end
+    puts @@all
+    # @@all.each do |name|
+    #   puts name
+    # end
   end
 
   def self.clear_all

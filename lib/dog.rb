5b5d60f54f27302abfed12d4eb776ddf91c3c2de
Kate Summerill <kaitlynsummerill@gmail.com>
2020-01-01 18:48:13 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -17,10 +17,7 @@ class Dog
     @@all
   end
 
-  # +#<Dog:0x0000000001a4cad0>
-  #    +#<Dog:0x0000000001a4ca30>
-  #    +#<Dog:0x0000000001a4c990>
-  # all array = +[#<Dog:0x000000000314a160 @name="Pluto">, #<Dog:0x000000000314a0c0 @name="Fido">, #<Dog:0x000000000314a020 @name="Maddy">]
+  
   def self.print_all
     # puts @@all.to_s
     @@all.each do |name|
